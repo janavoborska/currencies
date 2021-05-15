b69c329e5d987e509df86e27802201137cecf0e7
janavoborska <jana.dostalov@seznam.cz>
2021-05-15 16:41:00 +0200

    :reseni

--- src/components/Rate/index.jsx
+++ src/components/Rate/index.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+//import { ProgressPlugin } from 'webpack';
 import './style.css';
 
-const Rate = () => {
+const Rate = (props) => {
+  const [rate, setRate] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://api.frankfurter.app/latest?from=${props.from}&to=CZK`)
+      .then((resp) => resp.json())
+      .then((json) => {
+        setRate(json.rates.CZK);
+      });
+  }, [props.from]);
+
   return (
     <div className="rate">
-      <div className="rate__currency">1 USD</div>
+      <div className="rate__currency">1 {props.from}</div>
       <div>=</div>
-      <div className="rate__value">22 CZK</div>
+      <div className="rate__value">{rate} CZK</div>
     </div>
   );
 };
